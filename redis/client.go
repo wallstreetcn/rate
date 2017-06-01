8d74320d279d323818442ab10f6670246b8a6858
songrgg <songrgg0.0@gmail.com>
2017-06-02 00:30:30 +0800

    remove useless code

--- redis/client.go
+++ redis/client.go
@@ -3,8 +3,6 @@ package redis
 import (
 	"fmt"
 	"log"
-	"strconv"
-	"time"
 
 	redis "github.com/go-redis/redis"
 )
@@ -15,37 +13,6 @@ type ConfigRedis struct {
 	Port        int    `yaml:"port"`
 	Auth        string `yaml:"auth"`
 	IdleTimeout int    `yaml:"idle_timeout"`
-}
-
-// NewRedisRing constructs a redis ring.
-func NewRedisRing(config ...ConfigRedis) *redis.Ring {
-	servers := map[string]string{}
-	var pass string
-	var timeoutRead, timeoutWrite, timeoutConnect, timeoutIdle time.Duration
-
-	for i, v := range config {
-		servers[strconv.Itoa(i)] = fmt.Sprintf("%s:%d", v.Host, v.Port)
-		if i == 0 {
-			pass = v.Auth
-			timeoutIdle = time.Second * time.Duration(v.IdleTimeout)
-		}
-	}
-	client := redis.NewRing(&redis.RingOptions{
-		Addrs:        servers,
-		Password:     pass,
-		IdleTimeout:  timeoutIdle,
-		ReadTimeout:  timeoutRead,
-		WriteTimeout: timeoutWrite,
-		DialTimeout:  timeoutConnect,
-		PoolSize:     200,
-	})
-
-	if _, err := client.Ping().Result(); err != nil {
-		log.Println("fail to initialize redis ring: ", err)
-		client = nil
-	}
-
-	return client
 }
 
 // NewRedisClient constructs a redis client.
